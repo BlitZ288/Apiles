aedc4452ddcc58f8d38dd2bd9cfdd2ad5993284e
Blitz Blitz <raven3001@mail.ru>
2021-02-15 11:20:22 +0300

    Add project files.

---
+++ Apiles/Controllers/WindowController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading.Tasks;
+using Apiles.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Apiles.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WindowController : ControllerBase
+    {
+        BinaryFormatter formet = new BinaryFormatter();
+        // GET: api/Windows
+        [HttpGet]
+        public ActionResult<Windows> GetWindow()
+        {
+            using (FileStream file = new FileStream("Windows.dat", FileMode.OpenOrCreate))
+            {
+                if (file.Length != 0)
+                {
+                    return (ActionResult<Windows>)formet.Deserialize(file);
+                }
+                return null;
+
+            }
+        }
+
+        // GET: api/Windows/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Windows
+        [HttpPost]
+        public ActionResult<Windows> Post( Windows window)
+
+        {
+            using (FileStream file = new FileStream("Windows.dat", FileMode.OpenOrCreate))
+            {
+              
+                formet.Serialize(file, window);
+            }
+            return Ok(window);
+
+            
+        }
+
+        // PUT: api/Windows/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Apiles/Models/Windows.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Apiles.Models
+{
+    [Serializable]
+
+    /// <summary>
+    /// Класс окна 
+    // </summary>
+
+    public class Windows
+    {
+        /// <summary>
+        /// Класс окна 
+        // </summary>
+
+
+        public int Id { get; set; }
+        /// <summary>
+        /// Количество окон
+        /// </summary>
+        public int Number { get; set; }
+        /// <summary>
+        /// Ширина окна
+        /// </summary>
+        public double Width { get; set; }
+        /// <summary>
+        /// Длинна окна
+        /// </summary>
+        public double Lenght { get; set; }
+        /// <summary>
+        /// Площадь
+        /// </summary>
+        public double S { get; set; }
+        /// <summary>
+        /// Периметр 
+        /// </summary>
+        public double P { get; set; }
+        /// <summary>
+        /// Конечная стоимость
+        /// </summary>
+        public double Result { get; set; }
+
+    }
+}
